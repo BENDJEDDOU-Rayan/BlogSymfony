bca0f1644cbd4caf2b2cd69265dd9622f1c1cead
BENDJEDDOU-Rayan-2225011a <rayan.bendjeddou@etu.univ-amu.fr>
2025-02-13 10:59:37 +0100

    Création entity Post

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,16 +6,27 @@ use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 final class HomeController extends AbstractController
 {
+
+    public function __construct(
+        private HttpClientInterface $client,
+    ) {
+    }
+
     #[Route('/', name: 'home')]
     public function index(PostRepository $postRepository): Response
     {
-        $posts = $postRepository->findAllPost();
+        $response = $this->client->request(
+            'GET',
+            'http://127.0.0.1:8000/api/posts?page=1'
+        );
+        $posts = $response->toArray();
         return $this->render('home/index.html.twig', [
             'header_title' => 'Blog de Rayan',
-            'posts' => $posts
+            'posts' => $posts['member']
         ]);
     }
 }
